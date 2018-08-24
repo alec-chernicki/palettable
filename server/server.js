b5260456d563c0de905052cec31e5c5f3ac6bf9b
Alec Ortega <alecortega@live.com>
2018-08-24 00:45:53 -0400

    Blow up old folder

--- server/server.js
+++ server/server.js
@@ -1,40 +1,37 @@
-// @flow
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
-axios.defaults.baseURL = "http://www.colourlovers.com/api/palettes";
-axios.defaults.params = { format: "json" };
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+axios.defaults.baseURL = 'http://www.colourlovers.com/api/palettes';
+axios.defaults.params = { format: 'json' };
 
 // Create Express App
 const app = express();
 
 // Initialize Middleware
-app.set("port", process.env.PORT || 3001);
+app.set('port', process.env.PORT || 3001);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Controllers
-const randomController = require("./controllers/randomController");
-const changeController = require("./controllers/changeController");
-const imageController = require("./controllers/imageController");
+const randomController = require('./controllers/randomController');
+const changeController = require('./controllers/changeController');
+const imageController = require('./controllers/imageController');
 
 // Assign Routes and Controllers
-app.get("/api/image/:palette", imageController.drawImage);
-app.get("/api/random", randomController.getRandom);
+app.get('/api/image/:palette', imageController.drawImage);
+app.get('/api/random', randomController.getRandom);
 app.get(
-  "/api/change",
+  '/api/change',
   changeController.hasExactMatch,
   changeController.hasClosestHexMatch,
   randomController.getRandom
 );
 
-/**
- * Initialize Express server
- */
-app.listen(app.get("port"), () => {
+// Initialize Express server
+app.listen(app.get('port'), () => {
   console.log(
-    "Express server listening on port %d in %s mode",
-    app.get("port"),
-    app.get("env")
+    'Express server listening on port %d in %s mode',
+    app.get('port'),
+    app.get('env')
   );
 });
