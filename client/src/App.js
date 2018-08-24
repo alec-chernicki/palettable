33e691ab57c7b085dfd98c32761c71ec9c0a5e10
Alec Ortega <alecortega@live.com>
2018-08-24 00:45:40 -0400

    Blow up old folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,44 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// @flow
+import React from 'react';
+import url from '../../utils/url';
 
-class App extends Component {
+type Props = {
+  colors: [Object],
+};
+
+class App extends React.Component<Props> {
+  componentDidMount() {
+    const { requestRandomPalette, hydrateFromUrl } = this.props;
+    const paletteFromUrl = url.parseColors();
+
+    if (paletteFromUrl.length) {
+      return hydrateFromUrl(paletteFromUrl);
+    }
+
+    requestRandomPalette();
+  }
+
+  suppressBackspace = e => {
+    const event = e || window.event;
+    const target = event.target || event.srcElement;
+
+    if (event.keyCode === 8 && !/input|textarea/i.test(target.nodeName)) {
+      return false;
+    }
+    return true;
+  };
+
   render() {
+    const { colors, isFetching } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Title colors={colors} />
+        <VisibleOnboarding />
+        <div className="main-container">
+          <ColorList colors={colors} isFetching={isFetching} />
+          <FooterContainer />
+        </div>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
